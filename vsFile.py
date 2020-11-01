d5e29e905e0e9b99e83b705a32a4b748cf63cca9
Evan Knight <evanbwknight@gmail.com>
2020-11-01 14:43:00 -0800

    updated

--- vsFile.py
+++ vsFile.py
@@ -9,19 +9,25 @@ DIMENSIONS = [65, 10, 10]
 #read in and store data
 
 class Network(object):
+
     def __init__(self):
+
         self.num_layers = len(DIMENSIONS)
-        self.nodes = [] #array of 85 nodes with 4 different types
+        self.nodes #array of 85 nodes with 4 different types
+        self.connections #dictionary with node touples as key and weights as values
 
     def initializeNodes(self):
+
         #create 1 bias node first
         self.nodes.append(Node("bias"))
+
         for i in DIMENSIONS:
             #k is to reference node type
             k = 0
             for j in range(i):
                 #create j nodes of type k
                 if k == 0:
+
                     self.nodes.append(Node("input"))
                     #create j input nodes
                 if k == 1:
@@ -32,11 +38,56 @@ class Network(object):
                     self.nodes.append(Node("output"))
             k = k + 1
 
+    def initializeConnections(self):
+
+        for startNode in self.nodes:
+
+            if startNode.type == "bias":
+
+                for endNode in self.nodes:
+
+                    if endNode.type == "output":
+
+                        if not self.connections.contains(startNode, endNode):
+                            self.connections[startNode, endNode] = random.uniform(low=-0.15, high=0.15)
+
+                        
+            if startNode.type == "input":
+
+                for endNode in self.nodes:
+
+                    if endNode.type == "middle":
+
+                        if not self.connections.contains(startNode, endNode):
+                            self.connections[startNode, endNode] = random.uniform(low=-0.15, high=0.15)
+
+
+            if startNode.type == "output":
+
+                for endNode in self.nodes:
+
+                    if endNode.type == "output":
+
+                        if not self.connections.contains(startNode, endNode):
+                            self.connections[startNode, endNode] = random.uniform(low=-0.15, high=0.15)
+                     
+
+            if startNode.type == "middle":
+
+                for endNode in self.nodes:
+
+                    if endNode.type == "input" or "output":
+
+                        if not self.connections.contains(startNode, endNode):
+                            self.connections[startNode, endNode] = random.uniform(low=-0.15, high=0.15)
+
+                        
+"""
     def initialize_weights(self):
         for node in self.nodes:
             for connection in node.connections: #may not correctly reference node
                 connection.setValue(random.uniform(low=-0.15, high=0.15,))
-
+"""
             
         # self.weights
         # self.biases
@@ -45,57 +96,31 @@ class Network(object):
     #initialize weights and biases as relationa values of node objects
 
 class Node(object):
+
     def __init__(self, type):
-        self.weights = dict()
+ 
         self.bias
         self.type = type
-        self.connections
-        
-    def initializeConnections(self, node):
-        if type == "input":
-            for i in range(DIMENSIONS[1]):
-                self.weights[connection] = 0
-        if type == "middle":
 
-        if type == "output":
-            
-        if type == "bias":
+"""
 
+class biasNode(Node):
 
+    def __init__(self, bias):
 
-    #def intiialize connections
-    #def setValue
+        self.bias = bias
+"""
 
-#create node object class with:
-    #weight to every node it is connected to
-    #type: input, middle or output
+def main():
 
+    net = Network()
 
-#create example class
-    #the data read in
+    print(net.nodes) 
+
+    
+if __name__ == "__main__":
+    main()
+
+    
 
 
-
-
-
-
-
-
-
-
-
-
-# class Perceptron(object):
-
-#     def __init__(self, sizes):
-
-#         self.num_layers = len(sizes)
-#         self.sizes = sizes
-#         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-#         self.weights = [np.random.randn(y, x)]
-
-#         # for x, y in zip(sizes[:-1], sizes[1:])]
-#         # print(self.weights)
-
-# synaptic_weights = random.uniform(low=-1, high=1, size=(3, 1))
-#     return synaptic_weights
